b24f89759585a29521a48afc631be096d3c13250
YSNLR <sdet.yileri@gmail.com>
2023-04-08 15:24:57 +0300

    Base Page initial c variable

--- src/test/java/gitPractice/page/BasePage.java
+++ src/test/java/gitPractice/page/BasePage.java
@@ -4,7 +4,10 @@ public class BasePage {
     public static void main(String[] args) {
 
         System.out.println("Hello");
-        
+        int a = 1;
+        int b= 3;
+        int c= a+b;
+        System.out.println("c = " + c);
 
     }
 
